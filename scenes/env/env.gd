30d4aa1f240e66d3e3eab50db7208d24949eadf1
Alex Cannellos <ajc5645@gmail.com>
2025-08-29 21:43:24 -0400

    first

---
+++ scenes/env/env.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+@export_range(0.0, 1.0) var dayhour_normalized: float = 0.0
+#@export var daylight_color: Gradient
+#@export var skylight_curve: Curve
+#@export var sky_top_color: Gradient
+#@export var horizon_color: Gradient
+#@export var sky_bottom_color: Gradient
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
