af7c6f17ab888151f153c0a64f29ac70e14a8f2b
Alex Cannellos <ajc5645@gmail.com>
2025-10-14 23:02:12 -0400

    working ui2d 1014

--- scenes/game/scripts/pause.gd
+++ scenes/game/scripts/pause.gd
@@ -1,1 +1,30 @@
-extends Node
+extends Control
+
+func _unhandled_input(_event: InputEvent):
+	if Input.is_action_just_pressed("start"):
+		visible = !visible
+		get_tree().paused = !get_tree().paused
+		if get_tree().paused:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		else:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+	if get_tree().paused and Input.is_action_just_pressed("select"):
+		get_tree().quit()
+
+#func _input(event):
+	#if event.is_action_pressed("save"):
+		#data.write_save()
+		#shared_data.write_save()
+#
+#func _process(delta):
+	#data.update_playtime(delta)
+	#data.character.update_data(delta)
+		
+		#TODO fix panels, close any then quit menu
+		#for panel in get_tree().get_nodes_in_group("panels"):
+			#if panel.inventory_interface.visible:
+				#panel.toggle_inventory_interface()
+				#return
+			#else:
+				#get_tree().quit()
--- scripts/globals/scenes.gd
+++ scripts/globals/scenes.gd
@@ -1,1 +1,22 @@
 extends Node
+
+#var SCENE_XFER_SCENE = preload("res://scenes/ui/scene_xfer.tscn")
+#var scene_xfer: Control
+
+var scene_dictionary  = {
+	"main" : "res://scenes/game/main_menu.tscn",
+	"sandbox" : "res://scenes/game/sandbox.tscn",
+}
+
+var last_scene: String = ""
+var current_scene: String = ""
+
+#func _ready() -> void:
+	#scene_xfer = SCENE_XFER_SCENE.instantiate()
+	#add_child(scene_xfer)
+
+func change_scene(from_scene: String, to_scene: String) -> void:
+	last_scene = from_scene
+	var to_scene_path = scene_dictionary.get(to_scene)
+	current_scene = to_scene
+	get_tree().change_scene_to_file(to_scene_path)
