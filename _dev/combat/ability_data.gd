41317f0a8fc7789be17d12cc26620df3d2325256
Alex Cannellos <ajc5645@gmail.com>
2025-10-21 20:18:35 -0400

    dev combat input

---
+++ _dev/combat/ability_data.gd
@@ -0,0 +1,27 @@
+class_name Ability
+extends Resource
+
+@export var ability_name: String = "hatsu"
+@export_enum("on_press", "on_hold", "on_release") var input: String = "on_press"
+@export_enum("hitscan", "projectile") var hit_type: String = "hitscan"
+
+#@export_enum("punch", "beam", "wave", "ball") var type: String = "beam"
+#@export var is_rapid: bool = false
+#@export var can_charge: bool = false
+
+#@export_subgroup("Properties")
+@export_range(0.1, 5) var cooldown: float = 0.1  # Firerate
+@export_range(1, 200) var max_distance: int = 100  # Fire distance
+#@export_range(0, 100) var damage: float = 5  # Damage per hit
+#@export_range(0, 100) var damage_min: float = 5  # Damage per hit
+#@export_range(0, 100) var damage_max: float = 5  # Damage per hit
+#
+@export_range(0, 10) var target_spread: float = 0  # Spread of each shot
+@export_range(0, 5) var spawn_spread: float = 0  # Spread of each shot
+@export_range(1, 10) var shot_count: int = 1  # Amount of shots
+@export_range(0, 100) var knockback: int = 0  # Amount of knockback
+
+#@export_subgroup("other")
+#@export var sound_shoot: String  # Sound path
+#@export var ability_texture: Texture2D
+#@export var crosshair: Texture2D  # Image of crosshair on-screen
--- scenes/player/components/player_head.gd
+++ scenes/player/components/player_head.gd
@@ -8,11 +8,17 @@ extends PlayerComponent3D
 var max_deg: float = 90
 var sensitivity: float = 0.001
 
+#var prev_t : Transform3D
+#var current_t : Transform3D
+
 #func move_sub_camera() -> void:
 	#sub_camera.global_transform = camera.global_transform
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	#camera.physics_interpolation_mode = Node.PHYSICS_INTERPOLATION_MODE_OFF
+	#prev_t = camera.global_transform
+	#current_t = camera.global_transform
 
 func handle_camera_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
@@ -21,49 +27,33 @@ func handle_camera_input(event: InputEvent) -> void:
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-max_deg), deg_to_rad(max_deg))
 
 
-
-#extends Node
 #
-#@onready var Character : CharacterBody3D = self.get_node(^"CharacterBody3D")
-#@onready var Camera : Camera3D = self.get_node(^"CharacterBody3D/Camera3D")
-#
-#var PrevT : Transform3D
-#var ThisT : Transform3D
-#
-#func _ready() -> void:
-	#Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	#Camera.physics_interpolation_mode = Node.PHYSICS_INTERPOLATION_MODE_OFF
-	#PrevT = Camera.global_transform
-	#ThisT = Camera.global_transform
-	#return
 #
 #func _input(event: InputEvent) -> void:
-	#if(event is InputEventMouseMotion):
-		#var T := Transform3D()
-		#Camera.rotation.y -= (float(event.screen_relative.x) / float(self.get_window().size.x)) * 2.0
-		#Camera.rotation_degrees.x = clampf(rad_to_deg(Camera.rotation.x + ((float(-event.screen_relative.y) / float(self.get_window().size.y)) * 2.0)), -85.0, 85.0)
+	#if (event is InputEventMouseMotion):
+		#handle_camera_input(event)
+		#var t := Transform3D()
+		#camera.rotation.y -= (float(event.screen_relative.x) / float(self.get_window().size.x)) * 2.0
+		#camera.rotation_degrees.x = clampf(rad_to_deg(camera.rotation.x + ((float(-event.screen_relative.y) / float(self.get_window().size.y)) * 2.0)), -85.0, 85.0)
 	#return
-#
+##
 #func _process(delta: float) -> void:	
-	#var Interp := Engine.get_physics_interpolation_fraction()
-	#Camera.global_transform = Transform3D(
+	#var interp := Engine.get_physics_interpolation_fraction()
+	#camera.global_transform = Transform3D(
 		#Basis(
-			#PrevT.basis.get_rotation_quaternion().normalized().slerp(
-				#ThisT.basis.get_rotation_quaternion().normalized(),
-				#Interp
+			#prev_t.basis.get_rotation_quaternion().normalized().slerp(
+				#current_t.basis.get_rotation_quaternion().normalized(),
+				#interp
 			#)
 		#),
 		#Vector3(
-			#PrevT.origin.slerp(
-				#ThisT.origin,
-				#Interp
+			#prev_t.origin.slerp(
+				#current_t.origin,
+				#interp
 			#)
 		#)
 	#)
-	#return
-#
 #
 #func _physics_process(delta: float) -> void:
-	#PrevT = ThisT
-	#ThisT = Camera.global_transform
-	#return
+	#prev_t = current_t
+	#current_t = camera.global_transform
